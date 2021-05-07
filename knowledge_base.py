8b232100973a059836e4c42289e87d5d6597e1c6
LucasHynes <65365981+LucasHynes@users.noreply.github.com>
2021-05-07 14:36:47 -0700

    error control in advance of use

--- knowledge_base.py
+++ knowledge_base.py
@@ -1,2 +1,13 @@
 def tool_one(input):
     return input.reverse()
+
+def score(attempt, answer):
+
+    if not (isinstance(attempt, list)):
+        return "Attempt var is not type list!"
+    
+    if not (isinstance(answer, list)):
+        return "Answer var is not type list"
+
+    if len(attempt) != len(answer):
+        return "Error: length of the answers attempted to score have different lengths"
